41baf7ff1c5cc0e0fb7a1b229bda11302161ecc2
nic0michael <nicomichael2012@gmail.com>
2025-06-29 14:16:27 +0200

    using maven

--- src/main/java/za/co/morse/morsesender/Main.java
+++ src/main/java/za/co/morse/morsesender/Main.java
@@ -1,9 +1,8 @@
 package za.co.morse.morsesender;
 
-
 public class Main {
     public static void main(String[] args) {
-        MorseSender sender = new MorseSender(800, 8);
+        MorseSender sender = new MorseSender(700, 12);
         sender.sender();
     }
 }
